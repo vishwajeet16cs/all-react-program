730794aac4241377ab84a0c0241317c4cef4b642
vishwajeet kumar <1si16cs129@sit.ac.in>
2021-11-17 17:29:52 +0530

    hook useState

---
+++ react_work/src/component/HookIncrDecr.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Add(){
+   var arr=0;
+    const [name, setName] = React.useState(arr);
+    const incr=()=>{ 
+        setName(name+1)   
+    }
+    const decr=()=>{
+        if(name>=0){
+            setName(name-1)
+        }
+        
+    }
+    return<>
+    
+    {/* <h6>{}</h6> */}
+    <button onClick={incr}>incr</button>
+    <h6>{name}</h6>
+    <button onClick={decr}>decr</button> 
+    </>
+}
+
+export default Add;
