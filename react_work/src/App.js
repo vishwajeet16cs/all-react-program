ff2c05f2af9f433471820ae44e499b5dd21f9f18
vishwajeet kumar <1si16cs129@sit.ac.in>
2021-11-18 08:26:06 +0530

    done

--- react_work/src/App.js
+++ react_work/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './Component/Home';
 class App extends React.Component{
   render(){
 	  
     return(<>
-
+        <Home/>
       <h1>increament and dicr of a number stored in a variable </h1>
     
       
