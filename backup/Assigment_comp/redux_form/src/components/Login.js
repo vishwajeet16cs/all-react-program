dc57b9b045afbc8ec452fd9e17f235575757e8cd
vishwajeet16cs <1si16cs129@sit.ac.in>
2021-11-28 14:43:18 +0530

    assigment redux form+

---
+++ backup/Assigment_comp/redux_form/src/components/Login.js
@@ -0,0 +1,61 @@
+import React,{useState} from 'react';
+import {useDispatch} from "react-redux";
+import { login } from '../features/userSlice';
+import './login.css'
+const Login=()=>{
+    const [name,setName]= useState("");
+    const [email,setEmail]= useState("");
+    const [password,setPassword]= useState("");
+    const [storeEnter,setstoreEnter]=useState([]);
+    const dispatch=useDispatch();
+    const handlerSubmit=(e)=>{
+        e.preventDefault();
+         dispatch(login({
+             name:name,
+             email:email,
+             password:password,
+             loggedIn:true,
+         }))
+         const newdata={id:new Date().getTime().toString(),name:name,email:email,password:password}
+         setstoreEnter([...storeEnter,newdata]);//will all enter data
+
+    }
+    return(
+        <>
+        <div className="login">
+        <form className="login__form" onSubmit={(e)=>handlerSubmit(e)}>
+             <h1 style={{color:"#fff",animation: "glow 1s ease-in-out infinite alternate", }}>Login Form</h1>
+                <div className="form-group">
+            <div>
+                <input type="text" 
+                    name="name"
+                    placeholder="Enter Name"
+                    className="my-3"
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)} />
+            </div>
+            <div>
+                <input type="email" 
+                    name="email"
+                    placeholder="Enter Email"
+                    className="my-3"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)} />
+            </div>
+            <div>
+                <input type="password" 
+                    name="password"
+                    placeholder="Enter Password"
+                    className="my-3"
+                    value={password }
+                    onChange={(e)=>setPassword(e.target.value)} />
+            </div>
+            
+            <button type="submit" className="btn btn-danger my-3">login</button>
+            </div>
+        </form>
+        </div>
+        </>
+    )
+}
+export default Login;
---
+++ backup/Assigment_comp/redux_form/src/navbar/CompInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const CompInfo =()=>{
+    console.log("hello check");
+    return(
+        <>
+        <h1>Company:<span style={{color:"lightblue"}}>Geeksynergy Technologies Pvt Ltd</span></h1>
+        <h1>Address:<span style={{color:"lightblue"}}> Sanjayanagar, Bengaluru-56</span></h1>
+        <h1>Phone:<span style={{color:"lightblue"}}>XXXXXXXXX09</span></h1>
+        <h1>Email:<span style={{color:"lightblue"}}>XXXx@gmail.com</span></h1>
+        
+        </>
+    )
+}
+export default CompInfo;
