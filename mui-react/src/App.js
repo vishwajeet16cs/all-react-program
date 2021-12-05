3558459e0ed2d79cb12377cce50db52a7829694d
vishwajeet16cs <1si16cs129@sit.ac.in>
2021-12-05 16:24:31 +0530

    k

--- mui-react/src/App.js
+++ mui-react/src/App.js
@@ -1,12 +1,13 @@
 import { Button, ButtonGroup, Checkbox } from "@material-ui/core";
 import Delete from "@material-ui/icons/Delete"
 import ButtonEx from "./component/ButtonEx";
-import CheckBox from "./component/CheckBox";
+// import CheckBox from "./component/CheckBox";
 function App() {
   return (
     <>
+    
     <h1>Hello To Material UI</h1>
-    <CheckBox/>
+    {/* <CheckBox/> */}
       <ButtonEx/>
     </>
   );
--- mui-react/src/component/CheckBox.jsx
+++ mui-react/src/component/CheckBox.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {CheckBox} from '@material-ui/core'
+import {CheckBox} from '@material-ui/core';
 const CheckBox = () => {
     return (
         <div>
             {/* <h2>check it is working</h2> */}
+
         </div>
     )
 }
