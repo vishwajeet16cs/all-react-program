e3fdc3e64c1af36d5c5de0ae29d7173cf9d226bb
vishwajeet16cs <1si16cs129@sit.ac.in>
2021-12-17 11:57:51 +0530

    drawer MUi Simple + mobile

--- mui-react/src/App.js
+++ mui-react/src/App.js
@@ -1,112 +1,12 @@
-import { BoxComponent } from "./component/BoxComponent";
-import ButtonEx from "./component/ButtonEx";
-import CheckBoxEx from "./component/CheckBox";
-import { ContainerEx } from "./component/ContainerEx";
-import { GridEx } from "./component/GridEx";
-import { RadioButton } from "./component/RadioButton";
-import { RangeSlider } from "./component/RangeSlider";
-///-------------------------------------------------
-
-import { SliderEx } from "./component/SliderEx";
-import { SwitchEx } from "./component/SwitchEx";
-import TextFieldEx from "./component/TextField";
-import {MUICssInJs} from "./component/MUICssInJs"
-import {Container, Typography} from '@material-ui/core'
-import { PaperEx } from "./component/PaperEx";
-import { ColorsUseMui } from "./component/ColorsUseMui";
-import { IconsEx } from "./component/IconsEx";
-import { ButtonEx2M } from "./component/ButtonEx2M";
-import { UtilityFunction } from "./component/UtilityFunction";
-import { GridSysetm } from "./component/GridSystem";
-import { GridSysetm2 } from "./component/GredSystem2";
-import { FloatingActionButton } from "./component/FloatingActionButton";
-import { CardEx } from "./component/CardEx";
-import { TextFieldEx2 } from "./component/TextFieldEx2";
-import { RadioButtonEx } from "./component/inputComponent/RadioButtonEx";
-import { CheckBoxEx2 } from "./component/inputComponent/CheckBoxEx2";
-import { ListComp } from "./component/inputComponent/ListComp";
-import {TableMUI} from "./component/inputComponent/TableMUI"
-import { Appbar } from "./component/inputComponent/Appbar";
-import {Box} from "@material-ui/core"
-import Navbar from "./Navbar/Navbar";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from "./PageNav/Home";
-import About from "./PageNav/About";
-import Contact from "./PageNav/Contact";
-function App() {
+import { ClippedDrawer } from "./DrawerMUI/ClippedDrawer";
+import { PhoneDrawer } from "./DrawerMUI/PhoneDrawer";
+// import { SimpleDrawer } from "./DrawerMUI/SimpleDrawer";
+ function App() {
   return (
     <>
-    {/* <About/> */}
-    <BrowserRouter>
-      <Container disableGutters>
-        <Navbar/>
-        <Routes>
-          <Route exact path="/home" element={<Home/>}/>
-          <Route exact path="/contact" element={<Contact/>}/>
-          <Route exact path="/about" element={<About/>}/>
-        </Routes>
-      </Container>
-    </BrowserRouter>
-    <Box mt={9}>
-    <Typography color="primary" variant="h2" align="center">Hello To Material UI World</Typography>
-    <Typography color="blue" variant="h4" align="center">Be Responsive And Make Responsive</Typography>
-    </Box>
-    
-    <hr/>
-    <ContainerEx/>
-    <hr/>
-    <TextFieldEx2/>
-    <hr/>
-    <ListComp/>
-    <hr/>
-    {/* <Navbar/> */}
-    {/* <Appbar/> */}
-    {/* <h1>hello check</h1> */}
-    <hr/>
-    <TableMUI/>
-    <hr/>
-    <CheckBoxEx2/>
-    <hr/>
-    <RadioButtonEx/>
-    <hr/>
-    <CardEx/>
-    <hr/>
-    <ButtonEx2M/>
-    <hr/>
-    <FloatingActionButton/>
-    <hr/>
-    <GridSysetm2/>
-    <hr/>
-    <GridSysetm/>
-    <hr/>
-    <UtilityFunction/>
-    <hr/>
-    <ColorsUseMui/> 
-    <hr/>
-    <IconsEx/>
-    <hr/>
-    <PaperEx/>
-    <hr/>
-    <MUICssInJs/>
-    <hr/>
-    <BoxComponent/>
-    <hr/>
-    <GridEx/>
-    <hr/>
-    <SwitchEx/>
-    <hr/>
-  <RangeSlider/>
-  <hr/>  
-  <TextFieldEx/>
-  <hr/>
-    <SliderEx/><hr/>
-      <RadioButton/><hr/>
-    <CheckBoxEx/><hr/>
-      <ButtonEx/><hr/>
+    <ClippedDrawer/>
+    {/* <PhoneDrawer/> */}
+        {/* <SimpleDrawer/> */}
     </>
   );
 }
---
+++ mui-react/src/DrawerMUI/PhoneDrawer.jsx
@@ -0,0 +1,53 @@
+import React,{useState} from 'react'
+import { Box,Button,AppBar,Toolbar,Typography,IconButton,SwipeableDrawer,List,ListItem,ListItemText,CssBaseline,makeStyles,Icon} from '@material-ui/core'
+const useStyles = makeStyles((theme)=>({
+    menuButton:{
+        marginRight:theme.spacing(2),
+    },
+    titile:{
+        marginRight:"auto",
+    },
+    content:{
+        padding:theme.spacing(3),
+    },
+    drawer:{
+        width:250,  
+    }
+}));
+export const PhoneDrawer = () => {
+    const classes= useStyles();
+    const [open,setOpen]= useState(false)
+    return (
+        <div>
+            <CssBaseline>
+                <SwipeableDrawer open={open} onClose={()=>setOpen(false)}>
+                    <List disablPadding className={classes.drawer} >
+                        <ListItem button>
+                            <ListItemText primary="First Item"/>
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemText primary="Second Item"/>
+                        </ListItem>
+
+                    </List>
+                </SwipeableDrawer>
+                <AppBar position="static" color="secondary">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit"
+                        onClick={()=>setOpen(true)}>
+                        <Icon style={{fontSize:50}}>menu</Icon>
+                        </IconButton>
+                        <Typography variant='h6' className={classes.titile}>MUI❤️</Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+                <main className={classes.content}>
+                    <Typography variant='h2'>New Reactjs feature!</Typography>
+                    <Typography align='justify'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam error magni et temporibus labore ullam recusandae quis odio mollitia quibusdam! Optio pariatur a eveniet. Itaque expedita deleniti cum cupiditate explicabo assumenda quod officiis ipsam velit modi? Animi, voluptatibus voluptatum tempora non, dolorem aliquam iste deleniti impedit, recusandae soluta assumenda fugit.
+                    </Typography>
+                </main>
+            </CssBaseline>
+        </div>
+    )
+}
