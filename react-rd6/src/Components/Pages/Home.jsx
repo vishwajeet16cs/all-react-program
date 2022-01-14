e99a4f7f2ff616ce7cb933463e58916bd6808c17
vishwajeet16cs <1si16cs129@sit.ac.in>
2022-01-14 23:21:19 +0530

    json-server-done

--- react-rd6/src/Components/Pages/Home.jsx
+++ react-rd6/src/Components/Pages/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
 const Home = () => {
+    const [users,setUsers] = useState([]);
+    useEffect(() => {
+        loadUsers()
+    }, [users])
+    const loadUsers = async()=>{
+        const result = await axios.get("http://localhost:5000/users")
+        console.log("result here->",result); 
+    }
     return (
         <div className='container'>
+            <h6>{JSON.stringify(users)}</h6>
             <h1>Home Coponent</h1>
         </div>
     )
