a32dda9fc5f735db308c29a5b453f069eba7c4b3
vishwajeet16cs <1si16cs129@sit.ac.in>
2022-01-15 14:42:06 +0530

    all other commit

--- Redux-Demo/index.js
+++
@@ -1,8 +0,0 @@
-const BUY_CAKE = "BUY_CAKE";// string const for type
-// Action creater -- function which retun the action
-function buyCake(){
-    return {
-        type:BUY_CAKE,
-        info:"first redux action"
-    }  
-} 
