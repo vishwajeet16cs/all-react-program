3e2568cbf9d95c197f0b17efa3520e417142c416
vishwajeet16cs <1si16cs129@sit.ac.in>
2022-01-15 14:40:15 +0530

    form validation

---
+++ for-clone-project/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Formic from './Component/Formic'
+
+const App = () => {
+  return (
+    <div>
+      <Formic/>
+    </div>
+  )
+}
+
+export default App
---
+++ for-clone-project/src/Component/Formic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formic = () => {
+    return (
+        <div>
+            <h1>hello from Formic</h1>
+        </div>
+    )
+}
+
+export default Formic
